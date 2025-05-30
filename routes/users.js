6f8c38ea5af262989a788855df3cf3394e4f57eb
dheurvashia2594 <dhakeurashi7@gmail.com>
2025-05-30 07:34:31 -0400

    Initial commit

--- routes/users.js
+++ routes/users.js
@@ -25,28 +25,62 @@ let users = [
 
 // GET request: Retrieve all users
 router.get("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(users);
 });
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+ const email = req.params.email;
+ const user = users.filter(user => user.email == email);
+  res.send(user);
 });
 
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    //Static
+//   const newUSer = {
+//         firstName: "Urvashi",
+//         lastName: "Dhake",
+//         email:"urvitest@gamil.com",
+//         DOB:"21-03-1989",
+//   }
+
+// From request Query param
+newUSer = {
+    firstName: req.query.firstName,
+    lastName: req.query.lastName,
+    email:req.query.email,
+    DOB:req.query.DOB,
+}
+  users.push(newUSer);
+  res.send(`New User Added: ${req.query.firstName}`+ JSON.stringify(users));
 });
 
 
 // PUT request: Update the details of a user by email ID
 router.put("/:email", (req, res) => {
-  // Copy the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+
+  const findUser = users.filter(user => user.email == email);
+
+  if(findUser.length > 0)
+  {
+        let updateUser = findUser[0];
+
+        const dob = req.query.DOB;
+        const lastName = req.query.lastName;
+        if(dob)
+        {
+            updateUser.dob = dob;
+        }
+        if(lastName)
+        {
+            updateUser.lastName = lastName;
+        }
+
+  }
+  req.send("Updated Successfully "+ JSON.stringify(users));
 });
 
 
